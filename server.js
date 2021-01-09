4e001cc68d6a1dd877c2c6c1723a96c27b6d7a40
XXLZopes <afordm99@gmail.com>
2021-01-08 18:44:04 -0800

    added removeAttribute "readonly" when entering edit mode

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
 const express = require('express');
 const path = require('path');
-
+const { findById } = require('./lib/Notes')
 const app = express();
 const PORT = 3001;
+const notes = require('./db/db.json') 
 
 app.use(express.static('public'));
-
-const notes = require('./db/db.json') 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
@@ -20,6 +18,16 @@ app.get('/notes', (req, res) => {
 });
 app.get('/api/notes', (req, res) => {
     return res.json(notes)
+});
+
+//get note based on id
+app.get('/api/notes/:id', (req, res) => {
+    const result = findById(req.params.id, notes);
+    if(results){
+        res.json(result)
+    } else{
+        res.send(404);
+    }
 });
 
 app.post('/api/notes', (req, res) => {
