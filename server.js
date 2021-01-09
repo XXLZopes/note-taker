969662756cb92c32e96ba2e51d0a19c46ca59260
XXLZopes <afordm99@gmail.com>
2021-01-09 12:17:46 -0800

    heroku

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require("express");
 const path = require("path");
 const { findById } = require("./lib/Notes");
 const app = express();
-const PORT = 3001;
-const port = process.env.PORT || 3001;
+// const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const notes = require("./db/db.json");
 const { response } = require("express");
 
