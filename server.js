8c814018b7cc9758e547ee1778b884d7f9d2bb71
XXLZopes <afordm99@gmail.com>
2021-01-09 00:41:16 -0800

    debugged delete using forloop to reset id based on array index

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const path = require('path');
 const { findById } = require('./lib/Notes')
 const app = express();
 const PORT = 3001;
-const notes = require('./db/db.json') 
+const notes = require('./db/db.json'); 
+const { response } = require('express');
 
 app.use(express.static('public'));
 // Sets up the Express app to handle data parsing
@@ -16,19 +17,37 @@ app.get('/', (req, res) => {
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
+
 app.get('/api/notes', (req, res) => {
     return res.json(notes)
 });
 
 //get note based on id
-app.get('/api/notes/:id', (req, res) => {
-    const result = findById(req.params.id, notes);
+app.get('/aip/notes/:id', (req, res) => {
+    const results = findById(req.params.id, notes);
     if(results){
-        res.json(result)
+        res.json(results)
     } else{
         res.send(404);
     }
 });
+
+app.delete('/api/notes/:id', (req, res) => {
+    // let ids = JSON.parse(note.parentElement.getAttribute('data-note')).id;
+    console.log('deleting')
+
+    const updatedNotes = notes.splice(req.params.id, 1);
+
+    //Change id to the value of the current array index for every object in the array
+    notes.forEach((item, index)=>{
+        item.id = index.toString();
+       });
+
+    res.send(updatedNotes);
+
+    console.log(updatedNotes);
+    console.log('deleted')
+})
 
 app.post('/api/notes', (req, res) => {
     req.body.id = notes.length.toString();
