73b0b50e9ac7ae7807d111fc175dff5e8a5d01a6
XXLZopes <afordm99@gmail.com>
2021-01-09 12:03:57 -0800

    piushing to heroku

--- server.js
+++ server.js
@@ -1,64 +1,63 @@
-const express = require('express');
-const path = require('path');
-const { findById } = require('./lib/Notes')
+const express = require("express");
+const path = require("path");
+const { findById } = require("./lib/Notes");
 const app = express();
 const PORT = 3001;
-const notes = require('./db/db.json'); 
-const { response } = require('express');
+const notes = require("./db/db.json");
+const { response } = require("express");
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
 });
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/notes.html'));
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
-app.get('/api/notes', (req, res) => {
-    return res.json(notes)
+app.get("/api/notes", (req, res) => {
+  return res.json(notes);
 });
 
 //get note based on id
-app.get('/aip/notes/:id', (req, res) => {
-    const results = findById(req.params.id, notes);
-    if(results){
-        res.json(results)
-    } else{
-        res.send(404);
-    }
+app.get("/aip/notes/:id", (req, res) => {
+  const results = findById(req.params.id, notes);
+  if (results) {
+    res.json(results);
+  } else {
+    res.send(404);
+  }
 });
 
-app.delete('/api/notes/:id', (req, res) => {
-    // let ids = JSON.parse(note.parentElement.getAttribute('data-note')).id;
-    console.log('deleting')
+app.delete("/api/notes/:id", (req, res) => {
+  console.log("deleting");
 
-    const updatedNotes = notes.splice(req.params.id, 1);
+  const updatedNotes = notes.splice(req.params.id, 1);
 
-    //Change id to the value of the current array index for every object in the array
-    notes.forEach((item, index)=>{
-        item.id = index.toString();
-       });
+  //Change id to the value of the current array index for every object in the array
+  notes.forEach((note, index) => {
+    note.id = index.toString();
+  });
 
-    res.send(updatedNotes);
+  res.send(updatedNotes);
 
-    console.log(updatedNotes);
-    console.log('deleted')
-})
+  console.log(updatedNotes);
+  console.log("deleted");
+});
 
-app.post('/api/notes', (req, res) => {
-    req.body.id = notes.length.toString();
-    const newNotes = req.body;
+app.post("/api/notes", (req, res) => {
+  req.body.id = notes.length.toString();
+  const newNotes = req.body;
 
-    notes.push(newNotes);
-    res.json(newNotes);
-    
-    console.log(notes);
+  notes.push(newNotes);
+  res.json(newNotes);
+
+  console.log(notes);
 });
 
 app.listen(PORT, () => {
-    console.log(`App listening on PORT http://localhost:${PORT}`);
-  });
+  console.log(`App listening on PORT http://localhost:${PORT}`);
+});
